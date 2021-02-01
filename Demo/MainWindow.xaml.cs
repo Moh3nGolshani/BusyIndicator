dcfc0e2f5bffb474e4c8fa5508d2b5421de17444
Diyare <Codexsolutions2020@hotmail.com>
2021-02-01 13:19:15 -0800

    Add shared project library and move usage code part into the shared library.

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using BusyIndicator;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace Demo
 {
@@ -10,42 +7,6 @@ namespace Demo
         public MainWindow()
         {
             InitializeComponent();
-
-            IndicatorComboBox.SelectedIndex = 0;
-        }
-
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            BusyIndicator.IsBusy = true;
-            await Task.Delay(10000);
-            BusyIndicator.IsBusy = false;
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var item = (sender as ComboBox).SelectedValue;
-            int index = IndicatorComboBox.Items.IndexOf(item);
-
-            switch (index)
-            {
-                case 0:
-                    BusyIndicator.IndicatorType = IndicatorType.DashLoader;
-                    break;
-                case 1:
-                    BusyIndicator.IndicatorType = IndicatorType.DotLoader;
-                    break;
-                case 2:
-                    BusyIndicator.IndicatorType = IndicatorType.ProgressBar;
-                    break;
-                case 3:
-                    BusyIndicator.IndicatorType = IndicatorType.ProgressRing;
-                    break;
-                case 4:
-                    BusyIndicator.IndicatorType = IndicatorType.Spinner;
-                    break;
-                default:
-                    break;
-            }
         }
 
         private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
