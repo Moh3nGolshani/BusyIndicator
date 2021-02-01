9be3a60ac2e59d288854b818d907d877b975e155
Diyare <Codexsolutions2020@hotmail.com>
2021-02-01 13:30:25 -0800

    Move back shared code into main demo project.

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using BusyIndicator;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Demo
 {
@@ -7,11 +10,47 @@ namespace Demo
         public MainWindow()
         {
             InitializeComponent();
+            IndicatorComboBox.SelectedIndex = 0;
+
         }
 
         private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             DragMove();
         }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BusyIndicator.IsBusy = true;
+            await Task.Delay(10000);
+            BusyIndicator.IsBusy = false;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (sender as ComboBox).SelectedValue;
+            int index = IndicatorComboBox.Items.IndexOf(item);
+
+            switch (index)
+            {
+                case 0:
+                    BusyIndicator.IndicatorType = IndicatorType.DashLoader;
+                    break;
+                case 1:
+                    BusyIndicator.IndicatorType = IndicatorType.DotLoader;
+                    break;
+                case 2:
+                    BusyIndicator.IndicatorType = IndicatorType.ProgressBar;
+                    break;
+                case 3:
+                    BusyIndicator.IndicatorType = IndicatorType.ProgressRing;
+                    break;
+                case 4:
+                    BusyIndicator.IndicatorType = IndicatorType.Spinner;
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 }
