f5f75ce14d7d138a37ca8fd4dbd44bbbe1b2669a
Diyare <Codexsolutions2020@hotmail.com>
2021-02-22 07:26:45 -0800

    Add prefix for xaml namespace. Change namespace to repor url.

--- BusyIndicator/Properties/AssemblyInfo.cs
+++ BusyIndicator/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -13,7 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("BusyIndicator")]
 [assembly: AssemblyCulture("")]
-
+[assembly: XmlnsPrefix("https://github.com/Peoky/BusyIndicator", "busyIndicator")]
+[assembly: XmlnsDefinition("https://github.com/Peoky/BusyIndicator", "BusyIndicator")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
