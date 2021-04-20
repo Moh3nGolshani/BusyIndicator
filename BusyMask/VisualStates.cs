eb6ef9ab2f8ed32ba3295f30746b0faf854e19b1
Mohsen Golshani <golshani.mohsen@gmail.com>
2021-04-20 16:24:58 +0430

    Delete BusyMask directory

--- BusyMask/VisualStates.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BusyIndicator
-{
-    internal static partial class VisualStates
-    {
-        public const string GroupVisibility = "VisibilityStates";
-        public const string StateVisible = "Visible";
-        public const string StateHidden = "Hidden";
-    }
-}
