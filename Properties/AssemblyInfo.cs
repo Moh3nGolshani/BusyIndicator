cca5e12c7a2b4ca2755bfc0a3c6eba355d46f488
Mohsen Golshani <golshani.mohsen@gmail.com>
2021-04-20 15:57:52 +0430

    New Indicator => Grid

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BusyIndicator")]
+[assembly: AssemblyDescription("BusyIndicator for WPF - BusyIndicator is used to give an application user a visual indicator that a long running process is occurring.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mohsen Golshani")]
+[assembly: AssemblyProduct("BusyIndicator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("BusyIndicator")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be22fe0b-46f6-430b-b0b4-9386867e3db0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
